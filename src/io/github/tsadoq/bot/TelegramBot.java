bbd82d3ad7c7132e07ca4b6e2d202de5d774feea
matagyah <matteo.villosio@studenti.polito.it>
2018-06-01 09:06:29 +0200

    added bot class

---
+++ src/io/github/tsadoq/bot/TelegramBot.java
@@ -0,0 +1,31 @@
+//              .                       The information contained on current file (the "Service") is for general information purposes only.
+//              |\                      
+//              | \
+//              |  \                    In no event shall be liable for any special, direct, indirect, consequential, or incidental damages  
+//              |  |                    or any damages whatsoever, whether in an action of contract, negligence or other tort, arising out of
+//              |  |)                   or in connection with the use of the Service or the contents of the Service. reserves the right to make 
+//              |  | )                  additions, deletions, or modification to the contents on the Service at any time without prior notice.
+//              |  |_)
+//             /|  / /
+//            /_| /_/|                  © by Matteo Villosio. All rights reserved
+//            | | | ||
+//            | | | ||
+//            | | | ||\                 Informations and contacts @ tsadoq.github.io
+//           /| | | || \
+//          / | | | ||  \
+//         /  | | | ||   \
+//        /   | | | ||    \
+//       /    | | | ||     \
+//      /     | | | ||      \
+//      \  ___| | | ||___   /
+//       --   | | | ||\  ---
+//           /| | | |\  \
+//          / \_/ \_/ \___|
+//         /  |     |  \
+//         \__|     |__/
+
+
+package io.github.tsadoq.bot;
+
+public class TelegramBot {
+}
